91514c400c041699eb63d7d1acb03a6ca2508b7d
Vladislav Tankov <vdtankov@gmail.com>
2018-11-14 14:29:32 +0300

    Move to 1.1.3 SNAPSHOT version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import groovy.lang.GroovyObject
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 group = "tanvd.jetaudit"
-version = "1.1.2"
+version = "1.1.3-SNAPSHOT"
 
 val kotlinVersion = "1.3.0"
 
