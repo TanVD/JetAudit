1fab8204d31ee7ed2c1eeee35137f080f6757fe0
Vladislav Tankov <vdtankov@gmail.com>
2018-11-14 14:22:22 +0300

    Update aws-s3-sdk version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import groovy.lang.GroovyObject
 import org.jfrog.gradle.plugin.artifactory.dsl.PublisherConfig
 
 group = "tanvd.jetaudit"
-version = "1.1.2-SNAPSHOT"
+version = "1.1.2"
 
 val kotlinVersion = "1.3.0"
 
